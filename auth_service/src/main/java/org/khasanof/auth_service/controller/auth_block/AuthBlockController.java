9863e51f838a838b18b441ccc425d620c7793817
khasanof373 <khasanof373@gmail.com>
2022-09-23 20:49:53 +0500

    Add Auth block Controller

---
+++ auth_service/src/main/java/org/khasanof/auth_service/controller/auth_block/AuthBlockController.java
@@ -0,0 +1,60 @@
+package org.khasanof.auth_service.controller.auth_block;
+
+import org.khasanof.auth_service.controller.AbstractController;
+import org.khasanof.auth_service.criteria.auth_block.AuthBlockCriteria;
+import org.khasanof.auth_service.dto.auth_block.AuthBlockCreateDTO;
+import org.khasanof.auth_service.dto.auth_block.AuthBlockDetailDTO;
+import org.khasanof.auth_service.dto.auth_block.AuthBlockGetDTO;
+import org.khasanof.auth_service.dto.auth_block.AuthBlockUpdateDTO;
+import org.khasanof.auth_service.response.Data;
+import org.khasanof.auth_service.service.auth_block.AuthBlockService;
+import org.khasanof.auth_service.utils.BaseUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = BaseUtils.PATH + "/auth_block/*")
+public class AuthBlockController extends AbstractController<AuthBlockService> {
+
+    public AuthBlockController(AuthBlockService service) {
+        super(service);
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public ResponseEntity<Data<String>> create(@RequestBody AuthBlockCreateDTO dto) {
+        service.create(dto);
+        return new ResponseEntity<>(new Data<>("Successfully Created - Auth Block"), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
+    public ResponseEntity<Data<String>> update(@RequestBody AuthBlockUpdateDTO dto) {
+        service.update(dto);
+        return new ResponseEntity<>(new Data<>("Successfully Updated - Auth Block"), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Data<String>> delete(@PathVariable String id) {
+        service.delete(id);
+        return new ResponseEntity<>(new Data<>("Successfully Deleted - Auth Block"), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "get/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Data<AuthBlockGetDTO>> get(@PathVariable String id) {
+        return new ResponseEntity<>(new Data<>(service.get(id)), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "detail/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Data<AuthBlockDetailDTO>> detail(@PathVariable String id) {
+        return new ResponseEntity<>(new Data<>(service.detail(id)), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public ResponseEntity<Data<List<AuthBlockGetDTO>>> get(@Valid AuthBlockCriteria criteria) {
+        return new ResponseEntity<>(new Data<>(service.list(criteria)), HttpStatus.OK);
+    }
+
+}
--- auth_service/src/main/java/org/khasanof/auth_service/service/auth_block/AuthBlockService.java
+++ auth_service/src/main/java/org/khasanof/auth_service/service/auth_block/AuthBlockService.java
@@ -5,11 +5,13 @@ import org.khasanof.auth_service.dto.auth_block.AuthBlockCreateDTO;
 import org.khasanof.auth_service.dto.auth_block.AuthBlockDetailDTO;
 import org.khasanof.auth_service.dto.auth_block.AuthBlockGetDTO;
 import org.khasanof.auth_service.dto.auth_block.AuthBlockUpdateDTO;
+import org.khasanof.auth_service.service.BaseService;
 import org.khasanof.auth_service.service.GenericCUDService;
 import org.khasanof.auth_service.service.GenericGDLService;
 
 public interface AuthBlockService extends
         GenericCUDService<AuthBlockCreateDTO, AuthBlockUpdateDTO, String>,
-        GenericGDLService<AuthBlockGetDTO, AuthBlockDetailDTO, String, AuthBlockCriteria> {
+        GenericGDLService<AuthBlockGetDTO, AuthBlockDetailDTO, String, AuthBlockCriteria>,
+        BaseService {
 
 }
