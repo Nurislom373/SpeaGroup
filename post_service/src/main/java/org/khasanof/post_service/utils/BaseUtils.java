bce3549e775c90485a8b4a1164e3065bea7a685c
Nurislom373 <nurislomx72@gmail.com>
2022-10-02 22:53:19 +0500

    Add BaseUtils PATH #5706

--- post_service/src/main/java/org/khasanof/post_service/utils/BaseUtils.java
+++ post_service/src/main/java/org/khasanof/post_service/utils/BaseUtils.java
@@ -1,4 +1,7 @@
 package org.khasanof.post_service.utils;
 
 public class BaseUtils {
+    public static final String API = "/api";
+    public static final String VERSION = "/v1";
+    public static final String PATH = API + VERSION;
 }
