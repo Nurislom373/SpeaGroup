7452910e1bb08bcab545c7df954291e4fc1ec672
Nurislom373 <nurislomx72@gmail.com>
2022-10-02 20:59:29 +0500

    Post Service Add configs #5704

---
+++ post_service/src/test/java/org/khasanof/post_service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khasanof.post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
