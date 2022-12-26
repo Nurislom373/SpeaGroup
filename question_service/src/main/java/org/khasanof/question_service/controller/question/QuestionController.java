4387871f43b2d58ece5f45123cae5e79a0c92df7
Nurislom <nurislomx72@gmail.com>
2022-12-26 20:23:34 +0500

    wrote doc category and question APIs #5757

--- question_service/src/main/java/org/khasanof/question_service/controller/question/QuestionController.java
+++ question_service/src/main/java/org/khasanof/question_service/controller/question/QuestionController.java
@@ -1,5 +1,8 @@
 package org.khasanof.question_service.controller.question;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.khasanof.question_service.controller.AbstractController;
 import org.khasanof.question_service.criteria.question.QuestionCriteria;
 import org.khasanof.question_service.dto.question.QuestionCreateDTO;
@@ -33,34 +36,138 @@ public class QuestionController extends AbstractController<QuestionService> {
         super(service);
     }
 
+    @Operation(
+            summary = "Question Create API",
+            description = "This API is used to create a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Successfully Created - Question"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            )
+    })
     @RequestMapping(value = "create", method = RequestMethod.POST)
     public ResponseEntity<Data<String>> create(@Valid @RequestBody QuestionCreateDTO dto) {
         service.create(dto);
         return new ResponseEntity<>(new Data<>("Successfully Created - Question"), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Question Update API",
+            description = "This API is used to update a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Updated - Question"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Not Found"
+            )
+    })
     @RequestMapping(value = "update", method = RequestMethod.PUT)
     public ResponseEntity<Data<String>> update(@Valid @RequestBody QuestionUpdateDTO dto) {
         service.update(dto);
         return new ResponseEntity<>(new Data<>("Successfully Updated - Question"), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Question Delete API",
+            description = "This API is used to delete a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Successfully Deleted - Question"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Not Found"
+            )
+    })
     @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Data<String>> delete(@PathVariable String id) {
         service.delete(id);
         return new ResponseEntity<>(new Data<>("Successfully Deleted - Question"), HttpStatus.NO_CONTENT);
     }
 
+    @Operation(
+            summary = "Question Get API",
+            description = "This API is used to get a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Received - Question"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Not Found"
+            )
+    })
     @RequestMapping(value = "get/{id}", method = RequestMethod.GET)
     public ResponseEntity<Data<QuestionGetDTO>> get(@PathVariable String id) {
         return new ResponseEntity<>(new Data<>(service.get(id)), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Question Detail API",
+            description = "This API is used to get detail a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Received - Question"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Not Found"
+            )
+    })
     @RequestMapping(value = "detail/{id}", method = RequestMethod.GET)
     public ResponseEntity<Data<QuestionDetailDTO>> detail(@PathVariable String id) {
         return new ResponseEntity<>(new Data<>(service.detail(id)), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Question List API",
+            description = "This API is used to get list a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Received - Question"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Not Found"
+            )
+    })
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public ResponseEntity<Data<List<QuestionGetDTO>>> list(@Valid QuestionCriteria criteria) {
         return new ResponseEntity<>(new Data<>(service.list(criteria)), HttpStatus.OK);
--- question_service/src/main/java/org/khasanof/question_service/controller/question_category/QuestionCategoryController.java
+++ question_service/src/main/java/org/khasanof/question_service/controller/question_category/QuestionCategoryController.java
@@ -1,5 +1,8 @@
 package org.khasanof.question_service.controller.question_category;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.khasanof.question_service.controller.AbstractController;
 import org.khasanof.question_service.criteria.question_category.QuestionCategoryCriteria;
 import org.khasanof.question_service.dto.question_category.*;
@@ -30,34 +33,139 @@ public class QuestionCategoryController extends AbstractController<QuestionCateg
         super(service);
     }
 
+
+    @Operation(
+            summary = "Question Category Create API",
+            description = "This API is used to add a category to a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Added Category - Question Category"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Category Not Found"
+            )
+    })
     @RequestMapping(value = "create", method = RequestMethod.POST)
     public ResponseEntity<Data<String>> create(@Valid @RequestBody QuestionCategoryCreateDTO dto) {
         service.create(dto);
         return new ResponseEntity<>(new Data<>("Successfully Created - Question Category"), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Question Category Add Category API",
+            description = "This API is used to add a category to a question."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Added Category - Question Category"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Category Not Found"
+            )
+    })
     @RequestMapping(value = "addCategory", method = RequestMethod.PUT)
     public ResponseEntity<Data<String>> addCategory(@Valid @RequestBody QuestionCategoryAddDTO dto) {
         service.addCategory(dto);
         return new ResponseEntity<>(new Data<>("Successfully Added Category - Question Category"), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Question Category Delete Category API",
+            description = "This API is used to delete a question category."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Deleted Category - Question Category"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Category Not Found"
+            )
+    })
     @RequestMapping(value = "deleteCategory", method = RequestMethod.PUT)
     public ResponseEntity<Data<String>> deleteCategory(@Valid @RequestBody QuestionCategoryDeleteDTO dto) {
         service.deleteCategory(dto);
         return new ResponseEntity<>(new Data<>("Successfully Deleted Category - Question Category"), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Question Category Get API",
+            description = "This API is used to get a question category."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Received - Question Category"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Category Not Found"
+            )
+    })
     @RequestMapping(value = "get/{id}", method = RequestMethod.GET)
     public ResponseEntity<Data<QuestionCategoryGetDTO>> get(@PathVariable String id) {
         return new ResponseEntity<>(new Data<>(service.get(id)), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Question Category Detail API",
+            description = "This API is used to get detail a question category."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Received - Question Category"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Question Category Not Found"
+            )
+    })
     @RequestMapping(value = "detail/{id}", method = RequestMethod.GET)
     public ResponseEntity<Data<QuestionCategoryDetailDTO>> detail(@PathVariable String id) {
         return new ResponseEntity<>(new Data<>(service.detail(id)), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Question Category List API",
+            description = "This API is used to get list a question category."
+    )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully Received - Question Category"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid Validation Exception"
+            )
+    })
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public ResponseEntity<Data<List<QuestionCategoryGetDTO>>> list(@Valid QuestionCategoryCriteria criteria) {
         return new ResponseEntity<>(new Data<>(service.list(criteria)), HttpStatus.OK);
