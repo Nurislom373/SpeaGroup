5933f954e07b91f89e0874c27d93aaaa13e8a234
Nurislom <nurislomx72@gmail.com>
2023-01-01 22:20:23 +0500

    add space_service #185

---
+++ space_service/src/main/java/org/khasanof/space_service/SpaceServiceApplication.java
@@ -0,0 +1,13 @@
+package org.khasanof.space_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceServiceApplication.class, args);
+    }
+
+}
---
+++ space_service/src/test/java/org/khasanof/space_service/SpaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khasanof.space_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
